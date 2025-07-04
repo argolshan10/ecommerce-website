dc8b0416fada589cd7d63ec3f3313a74a3361455
Alireza <golshandeveloper@gmail.com>
2025-07-04 14:07:38 +0330

    4th july 2025

--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from pyexpat.errors import messages
+from django.contrib import messages
 from card.Card  import Card
-from payment.forms import PostingForm
+from payment.forms import PostingForm , PaymentForm
 from payment.models import PostingAddress
 
 
 # Create your views here.
+
+def billing_info(request):
+    if request.POST:
+        cart = Card(request)
+        cart_products = cart.get_prods()
+        quantities = cart.get_quants()
+        totals = cart.totals()
+
+        # check to see if user is logged in
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html', {'cart_products': cart_products,
+                                                                 'quantities': quantities,
+                                                                 'totals': totals,
+                                                                 'posting_info': request.POST,
+                                                                 'billing_form': billing_form ,})
+        else :
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities': quantities, 'totals': totals, 'posting_info': request.POST, 'billing_form': billing_form ,})
+        posting_form = request.POST
+        return render(request, 'payment/billing_info.html', {'cart_products': cart_products,
+                                                             'quantities': quantities,
+                                                             'totals': totals,
+                                                             'posting_form': posting_form, })
+    else :
+        messages.success(request , "Access Denied")
+        return redirect('home')
+
 def checkout(request):
     cart = Card(request)
     cart_products = cart.get_prods()
